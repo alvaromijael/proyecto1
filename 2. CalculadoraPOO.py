388bc252b30f31d069d1f1dfa3606f8d3de69fd3
alvaro19942003@hotmail.com <alvaro19942003@hotmail.com>
2022-08-17 00:19:19 -0500

    Version 1.1: version modificada

--- 2. CalculadoraPOO.py
+++ 2. CalculadoraPOO.py
@@ -30,6 +30,7 @@ class Calculadora:
         print("El resultado de la division es: ", div)
 
 print("Calculadora")
+print("Esto es un cambio")
 num1=float(input("Ingrese el primer número:"))   
 num2=float(input("Ingrese el segundo número:"))  
 
